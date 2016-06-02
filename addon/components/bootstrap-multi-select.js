2e7bff68111d0225231d2a050545242c35ae1c5a
Joe Weakley <joew@samjoe.com>
2016-06-02 10:08:46 -0700

    Added multi-select support.

---
+++ addon/components/bootstrap-multi-select.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import layout from '../templates/components/bootstrap-multi-select';
+import InputableMixin from '../mixins/components/inputable';
+
+export default Ember.Component.extend(InputableMixin, {
+  classNames: ['form-group', 'bootstrap-input-component'],
+  classNameBindings: ['hasSuccess:has-success', 'hasWarning:has-warning', 'hasError:has-error'],
+  layout: layout,
+
+  selected: null,
+  placeholder: null,
+  options: null,
+  onChange: null,
+  label: null,
+
+  disabled: false,
+  loadingMessage: null,
+  allowClear: false,
+  searchEnabled: false,
+  searchField: null,
+});
