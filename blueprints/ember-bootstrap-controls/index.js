f249d67332f6cec6a2a4dc8ad152fdd9e0c43367
Erin Schmidt <erin@wild.land>
2016-11-08 15:40:39 -0800

    ember input required style updates - removce ugly red box around input div

--- blueprints/ember-bootstrap-controls/index.js
+++ blueprints/ember-bootstrap-controls/index.js
@@ -12,6 +12,10 @@ module.exports = {
         {
           name: "ember-power-select",
           target: "0.10.3"
+        },
+        {
+          name: "ember-truth-helpers",
+          target: "1.2.0"
         }
       ]
     });
