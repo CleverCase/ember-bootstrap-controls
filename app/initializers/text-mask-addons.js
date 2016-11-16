efc2dc4a08f2d7a507db3e056b26051c4984c94b
Joe Weakley <jweakley@users.noreply.github.com>
2016-11-16 09:46:27 -0800

    Update text-mask-addons.js

--- app/initializers/text-mask-addons.js
+++ app/initializers/text-mask-addons.js
@@ -1,7 +1,7 @@
 import createNumberMask from 'npm:text-mask-addons/dist/createNumberMask';
 
 export default {
-  name: 'ember-dnd-core',
+  name: 'text-mask-addons',
   initialize() {
     console.assert(createNumberMask);
     // no-op, just loading dnd-core from browserify
