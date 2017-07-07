a708548a5ff315af3771203f9b7a867639cef883
Sam Clopton <samsinite@gmail.com>
2017-07-07 13:21:18 -0700

    remove util that I decided not to use

--- addon/utils/-register-helper.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-let registerHelper = Ember.Helper.helper ? Ember.Helper.helper : Ember.HTMLBars.makeBoundHelper;
-
-export default function registerHelper(helper) {
-  return registerHelper(helper);
-}
