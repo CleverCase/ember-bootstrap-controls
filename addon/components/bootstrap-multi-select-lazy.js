25e14322ed5f9951339acf45597b361dad9eb206
Sam Clopton <samsinite@gmail.com>
2017-10-24 13:11:53 -0700

    Add lazy multi-select (#97)
    This PR adds a lazy multi select to the control library and abstracts
    out the lazy-select state so that the majority of the code between the
    two components an be re-used simply.

--- addon/components/bootstrap-multi-select-lazy.js
+++ addon/components/bootstrap-multi-select-lazy.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
-import { POWER_SELECT_CLASS_NAME } from './bootstrap-power-select';
+import { MULTI_SELECT_CLASS_NAME } from './bootstrap-multi-select';
 import generateUUID from 'ember-bootstrap-controls/utils/generate-uuid';
-import layout from '../templates/components/bootstrap-power-select-lazy';
+import layout from '../templates/components/bootstrap-multi-select-lazy';
 import LazySelectState from '../utils/lazy-select-state';
 
 export default Ember.Component.extend({
   tagName: '', // We don't want a wrapping tag as this is a wrapper and the control-wrapper component will define the tag and bindings
   layout,
-  classNames: [POWER_SELECT_CLASS_NAME],
+  classNames: [MULTI_SELECT_CLASS_NAME],
 
   /* Required Attributes */
   searchByPage: undefined,
@@ -31,7 +31,7 @@ export default Ember.Component.extend({
       const searchByPage = this.get('searchByPage');
       const debounceMS = this.get('debounceMS');
 
-      Ember.assert('Must pass a `searchByPage action to `bootstrap-power-select-lazy`', searchByPage);
+      Ember.assert('Must pass a `searchByPage action to `bootstrap-multi-select-lazy`', searchByPage);
 
       return lazyState.search(term, searchByPage, debounceMS);
     }
