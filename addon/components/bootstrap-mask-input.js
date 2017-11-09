751b7239442c9de25f954860b26ea515d7ce2784
Sam Clopton <samsinite@gmail.com>
2017-11-08 16:24:33 -0800

    Fix masked input
    Previously, the value being passed into masked input was readonly,
    preventing it from being mutated on change. This PR removes that and
    switches this control over the HOC `bootstrap/control-wrapper`.

--- addon/components/bootstrap-mask-input.js
+++ addon/components/bootstrap-mask-input.js
@@ -1,47 +1,13 @@
 import Ember from 'ember';
 import layout from '../templates/components/bootstrap-mask-input';
-import InputableMixin from '../mixins/components/inputable';
 
-const { guidFor } = Ember;
+export const INPUT_MASK_CLASS_NAME = 'bootstrap-input-mask-component';
 
-export default Ember.Component.extend(InputableMixin, {
-  layout: layout,
+export default Ember.Component.extend({
   tagName: '',
-  classNames: '',
-  value: null,
-  readonly: null,
-  type: null,
-  srOnly: null,
-  tabindex: 0,
-  required: false,
-  disabled: false,
+  classNames: [INPUT_MASK_CLASS_NAME],
+  layout: layout,
 
-  mask: null,
   placeholderChar: '_',
-  placeholder: '',
   keepCharPositions: true,
-
-  guid: Ember.computed.readOnly('_guid'),
-  _guid: null,
-
-  /* action attrs */
-  'key-press': null,
-  'key-up': null,
-  'key-down': null,
-  'focus-out': null,
-
-  inputGuid: Ember.computed('guid', function() {
-    return `input-${this.get('guid')}`;
-  }),
-
-  hasValue: Ember.computed('value', function() {
-    const value = this.get('value');
-    return value ? true : false;
-  }),
-
-  init() {
-    this._super(...arguments);
-    const guid = guidFor(this);
-    this.set('_guid', guid);
-  }
 });
