7d659083a92e71a0cacd6a81c0d8bf055c6c7fbe
Kevin Haro <harokevin0@gmail.com>
2017-12-14 16:12:09 -0800

    Remove accidental wrapper around bootstrap-input (#106)
    * Remove accidental wrapper around bootstrap-input
    
    * Add placeholder to input mask

--- addon/components/bootstrap-input.js
+++ addon/components/bootstrap-input.js
@@ -5,15 +5,11 @@ import asserIfUsingRenamedEvents from '../utils/assert-if-using-renamed-events';
 
 export default Ember.Component.extend(InputableMixin, {
   layout: layout,
-  classNames: ['form-group', 'bootstrap-input-component'],
-  classNameBindings: ['hasSuccess:has-success', 'hasWarning:has-warning', 'showError:has-error'],
-  placeholder: null,
-  value: null,
-  readonly: null,
-  type: null,
-  srOnly: null,
+  tagName: '',
+
+  classNames: ['bootstrap-input-component'],
+
   tabindex: 0,
-  required: false,
 
   didReceiveAttrs() {
     this._super(...arguments);
