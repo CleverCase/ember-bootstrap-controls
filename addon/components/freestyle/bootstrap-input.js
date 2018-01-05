55efbff5dde3e79fd16c602646bda3172864da2c
Sam Clopton <samsinite@gmail.com>
2018-01-04 22:37:00 -0800

    add ability to wrap inputs

--- addon/components/freestyle/bootstrap-input.js
+++ addon/components/freestyle/bootstrap-input.js
@@ -4,22 +4,4 @@ import layout from '../../templates/components/freestyle/bootstrap-input';
 
 export default Ember.Component.extend({
   layout: layout,
-
-  value: "test",
-  inputId: 1,
-  type: "text",
-  readonly: false,
-  disabled: false,
-
-  actions: {
-    keyPress() {
-      console.log('key pressed');
-    },
-    keyUp() {
-      console.log('key up');
-    },
-    keyDown() {
-      console.log('key down');
-    },
-  },
 });
