9e5aaae0b641fcfc4239114a6a1f4f9d1e521c7f
Erin E Schmidt <erin.schmidt@gmail.com>
2018-02-14 15:12:03 -0800

    update ember bootstrap controls so that checkbox block yields somethi… (#121)
    * update ember bootstrap controls so that checkbox block yields something... for icons replacement of checkbox specifically.
    
    * Update README.md

--- addon/components/bootstrap/-input.js
+++ addon/components/bootstrap/-input.js
@@ -2,4 +2,12 @@ import Ember from 'ember';
 
 export default Ember.TextField.extend({
   classNames: ['form-control'],
+
+  click() {
+  	const onClick = this.get('onClick');
+
+  	if (onClick) {
+  		onClick(...arguments);
+  	}
+  }
 });
