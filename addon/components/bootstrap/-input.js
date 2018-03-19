093745775eae84a69494efe8e121c1d2ae324e00
Joe Weakley <jweakley@users.noreply.github.com>
2018-03-19 10:37:45 -0700

    Added help text for inputs (#125)

--- addon/components/bootstrap/-input.js
+++ addon/components/bootstrap/-input.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.TextField.extend({
   classNames: ['form-control'],
+  attributeBindings: ['aria-describedby'],
 
   click() {
   	const onClick = this.get('onClick');
