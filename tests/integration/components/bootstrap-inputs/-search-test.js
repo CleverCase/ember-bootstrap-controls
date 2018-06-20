83db6f41af0b26ceee145b5df5a81bfb75b614fe
Kevin Haro <harokevin0@gmail.com>
2018-06-20 13:47:00 -0700

    Adds the search input component

---
+++ tests/integration/components/bootstrap-inputs/-search-test.js
@@ -0,0 +1,31 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import a11yAudit from 'ember-a11y-testing/test-support/audit';
+
+moduleForComponent('bootstrap-inputs/-search', 'Integration | Component | bootstrap button', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+  // Example if your component had an `action` that it required:
+  // this.set('noop', () => {});
+  // this.render(hbs`{{bootstrap-inputs/-search action=noop}}`);
+  this.render(hbs`{{bootstrap-inputs/-search}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#bootstrap-inputs/-search}}
+      template block text
+    {{/bootstrap-inputs/-search}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+
+  return a11yAudit(this.$()).then(() => {
+    assert.ok(true, 'no a11y errors found!');
+  });
+});
