c7d04017df91025c9a76239f7e680bb0058773be
Joe Weakley <joew@samjoe.com>
2018-06-27 16:12:38 -0700

    New Radios and Checkboxes. Fixes #153 and #149.

--- addon/components/bootstrap/-checkbox.js
+++ addon/components/bootstrap/-checkbox.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Checkbox.extend({
-  classNames: ['form-control'],
+  classNames: ['form-check-input'],
 
   click() {
     const onClick = this.get('onClick');
 
     if (onClick) {
-      onClick(...arguments);
+      onClick(!this.get('checked'), this.get('checked'), ...arguments);
     }
   }
 });
--- tests/integration/components/freestyle/bootstrap/radio-group-test.js
+++ tests/integration/components/freestyle/bootstrap/radio-group-test.js
@@ -1,25 +1,23 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('bootstrap-radio-group', 'Integration | Component | bootstrap radio group', {
+moduleForComponent('freestyle/bootstrap/radio-group', 'Integration | Component | freestyle/bootstrap/radio group', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
-
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{bootstrap-radio-group}}`);
+  this.render(hbs`{{freestyle/bootstrap/radio-group}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#bootstrap-radio-group}}
+    {{#freestyle/bootstrap/radio-group}}
       template block text
-    {{/bootstrap-radio-group}}
+    {{/freestyle/bootstrap/radio-group}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
