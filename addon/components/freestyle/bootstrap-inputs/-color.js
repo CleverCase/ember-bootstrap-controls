cc51f17148d1f0e48b8fc2b0239555da4e3dfd20
Kevin Haro <harokevin0@gmail.com>
2018-06-27 17:15:37 -0700

    Generate color input component

---
+++ addon/components/freestyle/bootstrap-inputs/-color.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+import layout from '../../../templates/components/freestyle/bootstrap-inputs/-color';
+import { BuilderForPropDefaults } from 'ember-bootstrap-controls/utils/prop-definition-tools';
+import { propDefinitions } from '../../bootstrap-inputs/-color';
+
+export default Ember.Component.extend({
+  layout,
+  propDefinitions,
+  data: Object.assign(BuilderForPropDefaults(propDefinitions), {
+    value: '',
+  }),
+
+  basicValue: '',
+});
