721e2384708a154fca950ed47227d0c31c08e4c5
Kevin Haro <harokevin0@gmail.com>
2018-07-19 13:52:04 -0700

    Adds the {{bootstrap-inputs/time}} component

---
+++ addon/components/bootstrap-inputs/-time.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+import layout from '../../templates/components/bootstrap-inputs/-time';
+import { PropTypes } from 'ember-prop-types';
+import { BuilderForPropTypes, BuilderForPropDefaults } from 'ember-bootstrap-controls/utils/prop-definition-tools';
+
+export const propDefinitions = {
+  /*
+    autocomplete: {
+      default: 'email',
+      description: 'Indicates whether the value can be automatically completed by the browser.',
+      type: PropTypes.oneOf(['off', 'email']),
+    },
+  */
+};
+
+export default Ember.Component.extend({
+  layout,
+  propTypes: BuilderForPropTypes(propDefinitions),
+
+  getDefaultProps() {
+    return BuilderForPropDefaults(propDefinitions)
+  },
+});
