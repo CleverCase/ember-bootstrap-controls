5fe348f871c33383532b807fbc24def206cb1172
Kevin Haro <harokevin0@gmail.com>
2018-07-22 19:58:27 -0700

    Fix simeple select test

--- tests/integration/components/bootstrap-inputs/-select-test.js
+++ tests/integration/components/bootstrap-inputs/-select-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('bootstrap-inputs/-select', 'Integration | Component | bootstrap inputs/ select', {
+moduleForComponent('bootstrap-inputs/-select', 'Integration | Component | Bootstrap Inputs | Select', {
   integration: true
 });
 
@@ -9,13 +9,17 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{bootstrap-inputs/-select}}`);
+  this.set('fakeOnChangeAction', () => {});
+
+  this.render(hbs`{{bootstrap-inputs/-select onChange=(action fakeOnChangeAction)}}`);
 
   assert.equal(this.$().text().trim(), '');
 
+  this.set('selectOptions', ["template block text"]);
+
   // Template block usage:
   this.render(hbs`
-    {{#bootstrap-inputs/-select}}
+    {{#bootstrap-inputs/-select onChange=(action fakeOnChangeAction) options=selectOptions}}
       template block text
     {{/bootstrap-inputs/-select}}
   `);
