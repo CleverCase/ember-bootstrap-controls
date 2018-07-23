69976e4407f8e43ad823ae5a2e92a9a9a4edcbf1
Kevin Haro <harokevin0@gmail.com>
2018-07-22 20:34:09 -0700

    Fix form controls cancel button test

--- tests/integration/components/bootstrap/-form-controls/cancel-button-test.js
+++ tests/integration/components/bootstrap/-form-controls/cancel-button-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('bootstrap/-form-controls/cancel-button', 'Integration | Component | bootstrap/ form controls/cancel button', {
+moduleForComponent('bootstrap/-form-controls/cancel-button', 'Integration | Component | Bootstrap | Form Controls | Cancel Button', {
   integration: true
 });
 
@@ -11,7 +11,7 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{bootstrap/-form-controls/cancel-button}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), 'Cancel');
 
   // Template block usage:
   this.render(hbs`
