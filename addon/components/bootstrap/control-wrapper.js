bc8b498a97b1cd332b1ed551a5cc92f0d2690c4a
Erin Schmidt <erin@wild.land>
2018-07-24 10:51:15 -0700

    update contributing guidlines

--- addon/components/bootstrap/control-wrapper.js
+++ addon/components/bootstrap/control-wrapper.js
@@ -4,4 +4,5 @@ import layout from '../../templates/components/bootstrap/control-wrapper';
 
 export default Ember.Component.extend(InputableMixin, {
   layout,
+  classNameBindings: ['floatingLabel:form-label-group'],
 });
