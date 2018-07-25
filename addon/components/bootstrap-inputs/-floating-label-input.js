7b9723c2d323fcebac5d78742a307613c91c51f7
Erin Schmidt <erin@wild.land>
2018-07-25 14:29:54 -0700

    everything needed for a floating label input component

--- addon/components/bootstrap-inputs/-floating-label-input.js
+++ addon/components/bootstrap-inputs/-floating-label-input.js
@@ -1,24 +1,9 @@
 import Ember from 'ember';
-import layout from '../../templates/components/bootstrap-inputs/-text';
+import layout from '../../templates/components/bootstrap-inputs/-floating-label-input';
 import { PropTypes } from 'ember-prop-types';
 import { BuilderForPropTypes, BuilderForPropDefaults } from 'ember-bootstrap-controls/utils/prop-definition-tools';
-const autoCompleteTypes = ['off', 'on', 'name', 'honorific-prefix',
-  'given-name', 'additional-name', 'family-name', 'honorific-suffix',
-  'nickname', 'username', 'organization-title', 'organization',
-  'street-address', 'address-line1',
-  'address-line2', 'address-line3', 'address-level4', 'address-level3',
-  'address-level2', 'address-level1', 'country', 'country-name', 'postal-code',
-  'cc-name', 'cc-given-name', 'cc-additional-name', 'cc-family-name',
-  'cc-number', 'cc-exp', 'cc-exp-month', 'cc-exp-year', 'cc-csc', 'cc-type',
-  'transaction-currency', 'transaction-amount', 'language', 'bday', 'bday-day',
-  'bday-month', 'bday-year', 'sex'];
 
 export const propDefinitions = {
-  autocomplete: {
-    default: 'on',
-    description: 'Indicates whether the value can be automatically completed by the browser.',
-    type: PropTypes.oneOf(autoCompleteTypes),
-  },
   disabled: {
     description: 'Indicates whether the control is disabled',
     type: PropTypes.bool,
@@ -47,10 +32,6 @@ export const propDefinitions = {
     description: "A regular expression that the control's value is checked against.",
     type: PropTypes.instanceOf(RegExp),
   },
-  placeholder: {
-    description: 'A hint to the user of what can be entered in the control. This is displayed in the empty input.',
-    type: PropTypes.string,
-  },
   readonly: {
     default: false,
     description: 'Indicates that the user cannot modify the value of the control.',
@@ -59,11 +40,6 @@ export const propDefinitions = {
   required: {
     default: false,
     description: 'Indicates that the user must fill in a value before submitting a form.',
-    type: PropTypes.bool,
-  },
-  srOnly: {
-    default: false,
-    description: 'Indicated that the label should be hidden to all devices except screen readers',
     type: PropTypes.bool,
   },
   tabindex: {
@@ -78,6 +54,7 @@ export const propDefinitions = {
 
 export default Ember.Component.extend({
   layout,
+  tagName: '',
   propTypes: BuilderForPropTypes(propDefinitions),
 
   getDefaultProps() {
