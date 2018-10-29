baf99b059bf9b479d11436556afa4c01696a7ead
Joe Weakley <joew@samjoe.com>
2018-10-29 15:18:12 -0700

    Removed tags.

--- addon/components/bootstrap/form/-inputs.js
+++ addon/components/bootstrap/form/-inputs.js
@@ -2,5 +2,6 @@ import Component from '@ember/component';
 import layout from '../../../templates/components/bootstrap/form/-inputs';
 
 export default Component.extend({
-  layout
+  layout,
+  tagName: '',
 });
