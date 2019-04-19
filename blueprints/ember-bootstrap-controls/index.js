c1da1fdd186370dca2fb1745b253065a0a23167e
flarinerin <erin.schmidt@gmail.com>
2019-04-19 15:20:00 -0700

    edits to power select appearance, making them align more with bootsrap 4

--- blueprints/ember-bootstrap-controls/index.js
+++ blueprints/ember-bootstrap-controls/index.js
@@ -1,4 +1,33 @@
 module.exports = {
   description: 'Ember Bootstrap Controls Install blueprint',
   normalizeEntityName: function() {},
+
+  afterInstall() {
+    return this.addAddonsToProject({ packages: ['ember-power-select'] })
+      .then(() => this.addPreprocessorStyleImport());
+  },
+  addPreprocessorStyleImport() {
+    let preprocessor = this.preprocessor;
+    let importStatement = '\n@import "bootstrap-power-select/themes/bootstrap-4";\n';
+
+    if (preprocessor === 'none') {
+      return;
+    }
+
+    let extension = preprocessor === 'sass' ? 'scss' : 'less';
+
+    let stylePath = path.join('app', 'styles');
+    let file = path.join(stylePath, `app.${extension}`);
+
+    if (!fs.existsSync(stylePath)) {
+      fs.mkdirSync(stylePath);
+    }
+    if (fs.existsSync(file)) {
+      this.ui.writeLine(`Added import statement to ${file}`);
+      return this.insertIntoFile(file, importStatement, { before: '@import "ember-power-select";' });
+    } else {
+      this.ui.writeLine(`Created ${file}`);
+      return writeFile(file, importStatement);
+    }
+  },
 };
