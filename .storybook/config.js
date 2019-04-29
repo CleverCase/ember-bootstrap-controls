c840edaad6cb0e643084940d764260a3e095fc95
flarinerin <erin.schmidt@gmail.com>
2019-04-29 13:53:20 -0700

    Added more addons.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,12 @@
-import { configure } from '@storybook/ember';
+import { configure, addDecorator } from '@storybook/ember';
+import { withA11y } from '@storybook/addon-a11y';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(withA11y);
 
 configure(loadStories, module);
--- stories/components/buttons/accept-button.stories.js
+++ stories/components/buttons/accept-button.stories.js
@@ -3,12 +3,37 @@ import hbs from 'htmlbars-inline-precompile';
 import { storiesOf } from '@storybook/ember';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { text, withKnobs } from '@storybook/addon-knobs';
 
-storiesOf('buttons | accept-button', module).add('without text', () => ({
-  template: hbs`{{bootstrap/buttons/accept-button
-    action=onClick
-  }}`,
-  context: {
-    onClick: action('clicked'),
-  },
-}));
+
+const stories = storiesOf('buttons | accept-button', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add(
+  'without text',
+  () => ({
+    template: hbs`{{bootstrap/buttons/accept-button
+      action=onClick
+    }}`,
+    context: {
+      onClick: action('clicked')
+    },
+  }),
+  {
+    notes: 'Super simple notes?'
+  }
+);
+stories.add(
+  'with text',
+  () => ({
+    template: hbs`{{bootstrap/buttons/accept-button
+      action=onClick
+      buttonText=buttonText
+    }}`,
+    context: {
+      onClick: action('clicked'),
+      buttonText: text('Button Text', 'Yes')
+    },
+  }),
+);
